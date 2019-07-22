aed6fffeac1c0300e36b457dee5433f0179e9290
zach kramer <zkramer22@gmail.com>
2019-07-22 15:03:08 -0400

    Updates

---
+++ precache-manifest.414938f6d0e7042a7738268b182f037e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57cda871c173ec5ee787a61d5ca0bc3b",
+    "url": "/hum-nutrition/index.html"
+  },
+  {
+    "revision": "cca075dce2a5f4eb7bef",
+    "url": "/hum-nutrition/static/css/main.d135e343.chunk.css"
+  },
+  {
+    "revision": "5a896bec2100e4ee8c92",
+    "url": "/hum-nutrition/static/js/2.97cd066a.chunk.js"
+  },
+  {
+    "revision": "cca075dce2a5f4eb7bef",
+    "url": "/hum-nutrition/static/js/main.6439c990.chunk.js"
+  },
+  {
+    "revision": "32ec69ae8807317904fc",
+    "url": "/hum-nutrition/static/js/runtime~main.866aed10.js"
+  },
+  {
+    "revision": "e999ba4e0118aa852ce49e0a44857340",
+    "url": "/hum-nutrition/static/media/NeutraText-Book.e999ba4e.ttf"
+  },
+  {
+    "revision": "0dc98fdb485299b51076a7ba01a4e9d0",
+    "url": "/hum-nutrition/static/media/NeutraText-DemiAlt.0dc98fdb.otf"
+  },
+  {
+    "revision": "ac51ad8a36d5bab6da11d454bcd06561",
+    "url": "/hum-nutrition/static/media/ProximaNova-Light.ac51ad8a.otf"
+  },
+  {
+    "revision": "bf9f5d50c1b928ff21436517a1a95ad9",
+    "url": "/hum-nutrition/static/media/ProximaNova-Regular.bf9f5d50.otf"
+  },
+  {
+    "revision": "88e122578c579b60c6b3125b0471c995",
+    "url": "/hum-nutrition/static/media/hero-desktop.88e12257.png"
+  },
+  {
+    "revision": "d3304c2a50339ec66a55665fa3812246",
+    "url": "/hum-nutrition/static/media/hero.d3304c2a.png"
+  },
+  {
+    "revision": "e786bd9ca933d347c63a58e06fe54477",
+    "url": "/hum-nutrition/static/media/hum-logo.e786bd9c.png"
+  },
+  {
+    "revision": "2cb128ba003ea107ba9875b18c1a412d",
+    "url": "/hum-nutrition/static/media/orange-box.2cb128ba.png"
+  }
+]);
